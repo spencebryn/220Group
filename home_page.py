d039c4b307814d7c301633eedee2c81b53bee2ac
Spencer Strange <111928082+spencebryn@users.noreply.github.com>
2022-09-29 21:53:37 -0400

    Add files via upload

--- home_page.py
+++ home_page.py
@@ -1,8 +1,9 @@
+from cProfile import label
 from cgitb import text
 from distutils.cmd import Command
 from tkinter import *
 import tkinter as Tk
-from turtle import bgcolor
+from turtle import back, bgcolor
 from tkmacosx import Button
 from PIL import ImageTk, Image
 
@@ -12,16 +13,60 @@ root.resizable
 root.state("zoomed")
 root.configure(background="#000030")
 
-comicBookPic = Image.open("comic_book.jpeg")
-resizedComicBookPic = comicBookPic.resize((200, 200))
-comicBookImage = ImageTk.PhotoImage(resizedComicBookPic)
 
-comicBookImageButton = Label(
-    image=comicBookImage).grid(row=0, column=2)
-comicsButton = Button(root, text="Comics", bg="gold", fg="#000030", width=100)
-comicsButton.grid(row=1, column=2)
-boardgamesButton = Button(root, text="Boardgames",
-                          bg="gold", fg="#000030", width=100)
-boardgamesButton.grid(row=0, column=3)
+def openMarvel():
+    newWindow = Toplevel(root)
+    width = newWindow.winfo_screenwidth()
+    height = newWindow.winfo_screenheight()
+    newWindow.title("Marvel Comics")
+    newWindow.resizable
+    newWindow.geometry("%dx%d" % (width, height))
+
+
+def openDC():
+    newDCWindow = Toplevel(root)
+    width = newDCWindow.winfo_screenwidth()
+    height = newDCWindow.winfo_screenheight()
+    newDCWindow.title("DC Comics")
+    newDCWindow.resizable
+    newDCWindow.geometry("%dx%d" % (width, height))
+
+
+# Create Images
+marvelPic = Image.open("Marvel.jpeg")
+DCpic = Image.open("DC.jpeg")
+valiantpic = Image.open("Valiant.jpeg")
+darkhorsePic = Image.open("DarkHorse.jpeg.png")
+resizedDarkhorsePic = darkhorsePic.resize((250, 200))
+resizedValiantPic = valiantpic.resize((350, 200))
+resizedDCpic = DCpic.resize((350, 200))
+resizedMarvelPic = marvelPic.resize((350, 200))
+
+darkhorseImage = ImageTk.PhotoImage(resizedDarkhorsePic)
+DCImage = ImageTk.PhotoImage(resizedDCpic)
+marvelImage = ImageTk.PhotoImage(resizedMarvelPic)
+valiantImage = ImageTk.PhotoImage(resizedValiantPic)
+
+# create Labels / put on window
+
+marvelLabel = Label(image=marvelImage).grid(row=0, column=2)
+DCLabel = Label(image=DCImage).grid(row=0, column=3)
+valiantLabel = Label(image=valiantImage).grid(row=0, column=4)
+darkhorseLabel = Label(image=darkhorseImage).grid(row=0, column=5)
+
+# create Buttons / put on window
+
+
+MarvelButton = Button(root, text="Marvel", bg="gold",
+                      fg="#000030", width=100, command=openMarvel)
+MarvelButton.grid(row=1, column=2)
+DCButton = Button(root, text="DC Comics",
+                  bg="gold", fg="#000030", width=100, command=openDC)
+DCButton.grid(row=1, column=3)
+valiantButton = Button(root, text="Valiant", bg="gold",
+                       fg="#000030", width=100).grid(row=1, column=4)
+darkhorseButton = Button(root, text="Dark Horse", bg="gold",
+                         fg="#000030", width=100).grid(row=1, column=5)
+
 
 root.mainloop()
